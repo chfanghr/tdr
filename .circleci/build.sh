40fe608327ee4f58979e3a689eec3c1e7eb08211
fanghr <chfanghr@gmail.com>
2019-04-10 21:37:39 +0800

    update build script

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -4,18 +4,18 @@ sudo mkdir /build
 sudo chmod +666 /build
 GO111MODULE=on go get -d ./...
 
-go_build(){
+go_build_target(){
 GO111MODULE=on GOOS=$1 GOARCH=$2 go build -o /build/$1_$2 $3
 }
 
-go_build "linux" "amd64" $1
-go_build "linux" "386" $1
-go_build "linux" "arm" $1
-go_build "linux" "arm64" $1
-go_build "windows" "amd64" $1
-go_build "windows" "386" $1
-go_build "darwin" "amd64" $1
-go_build "darwin" "386" $1
+go_build_target "linux" "amd64" $1
+go_build_target "linux" "386" $1
+go_build_target "linux" "arm" $1
+go_build_target "linux" "arm64" $1
+go_build_target "windows" "amd64" $1
+go_build_target "windows" "386" $1
+go_build_target "darwin" "amd64" $1
+go_build_target "darwin" "386" $1
 
 tar -cf /tmp/build.tar /build
 mv /tmp/build.tar /build
