97e32fc604303d2a8caf284b89843e8479533439
fanghr <chfanghr@gmail.com>
2019-04-11 14:07:01 +0800

    clean up code

--- main.go
+++ main.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"github.com/chfanghr/tdr/spotify/utils"
-	"log"
-)
-
 func main() {
-	log.Println(utils.UnwrapResultFromJob(func() {
-		utils.ThrowData(1)
-	}))
+	//TODO
 }
